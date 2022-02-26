331b37f6aacfac082076e1a6179c7b5e304d1f0f
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2022-02-25 21:39:22 -0300

    First Commit

---
+++ Hello-JavaScript/script.js
@@ -0,0 +1,18 @@
+var texto01;
+var texto02;
+
+var numreal;
+var numinteiro;
+
+texto01 = "Hello World ";
+texto02 = "Java Script"
+numreal = 10.12;
+numinteiro = 20;
+
+document.write(texto01 + texto02);
+document.write("<br>Número Real: " + numreal);
+
+document.write("<br>Número Inteiro: " + numinteiro);
+
+
+alert("Acabei a atividade");
